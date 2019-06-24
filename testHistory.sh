f0bf9dd4c7cd4215be04b1e89c2b0d7034081388
shiny-mountain <shiny-mountain@lobstr.co>
2019-06-24 13:44:19 +0300

    initial commit

---
+++ testHistory.sh
@@ -0,0 +1,5 @@
+node localHistory.js
+echo '================================================================'
+echo
+echo
+ls output/v1/history
---
+++ testTicker.sh
@@ -0,0 +1,7 @@
+rm output/v1/ticker.json
+node localTicker.js
+echo '================================================================'
+echo
+echo
+cat output/v1/ticker.json
+echo
