954f5a22082197bac713ee8af1ef828fd41de305
shiny-mountain <shiny-mountain@lobstr.co>
2019-06-25 11:33:19 +0300

    Added index.js, added tickerDataGenerator, code prettify

---
+++ index.js
@@ -0,0 +1,4 @@
+const { tickerGenerator, tickerDataGenerator } = require('./functions/ticker');
+const historyGenerator = require('./functions/history');
+
+module.exports = { tickerGenerator, tickerDataGenerator, historyGenerator };
