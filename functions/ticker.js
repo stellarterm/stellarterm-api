cb6ab35a1893b217152a337a032008db029d20c0
flat-resonanse <flat-resonance@stellarterm.com>
2021-04-19 18:17:10 +0300

    Add reset call on ticker generation

--- functions/ticker.js
+++ functions/ticker.js
@@ -155,7 +155,9 @@ function loadAssets(ticker) {
         price_XLM: 1,
         price_USD: ticker._meta.externalPrices.USD_XLM,
     });
+    directory.reset();
     return directory.initializeAnchors(ANCHORS_SERVER).then(() => {
+        ticker._meta.build_id = directory.buildID;
         _.each(directory.assets, (asset, id) => {
             let r = {};
             r.id = id;
